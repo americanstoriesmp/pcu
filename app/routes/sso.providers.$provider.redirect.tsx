7bf83d8d4a9db7ac2edb25577ae0d81988ddebe7
François López <francois@piscesmetadata.com>
2024-10-07 23:54:07 -0500

    feat: sign in with google

---
+++ app/routes/sso.providers.$provider.redirect.tsx
@@ -0,0 +1,29 @@
+import { LoaderFunction, LoaderFunctionArgs } from '@remix-run/node';
+
+export const loader: LoaderFunction = async ({
+	params,
+	request,
+}: LoaderFunctionArgs) => {
+	const { provider } = params;
+	const url = new URL(request.url);
+	const id_token = url.searchParams.get('id_token');
+
+	const response = await fetch(
+		`http://localhost:1337/api/auth/${provider}/callback?access_token=${id_token}`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+
+	const data = await response.json();
+
+	return new Response(JSON.stringify(data), {
+		status: response.status,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+};
