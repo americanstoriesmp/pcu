95c2ff07bead9c9f61454aafdad55bd0aacd936c
François López <francois@piscesmetadata.com>
2024-10-14 19:09:32 -0500

    feat: strapi session data contract

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,10 +3,23 @@ import { GoogleProfile } from 'remix-auth-google';
 declare global {
 	type Profile = Pick<GoogleProfile, '_json'>;
 
+	type StrapiUser = {
+		username: string;
+		email: string;
+	};
+
 	interface CreatedSession {
 		profile: Profile;
 		storedInDatabase: boolean;
 		backendJwt: string;
 		backendIdentity: string;
+	}
+
+	interface StrapiSession {
+		jwt: string;
+		user: StrapiUser;
+		oauth?: boolean;
+		persist?: boolean;
+		setupFinished?: boolean; // this property tells us if the user has finished the setup process after oauth. If not, we will redirect them to the setup page.
 	}
 }
