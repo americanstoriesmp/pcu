ebd8980efe6c020f28cd4042e9ba7b94110cfee2
François López <francois@piscesmetadata.com>
2024-10-16 17:57:15 -0500

    feat: append user state setup to session

--- app/routes/auth.$provider.redirect/route.tsx
+++ app/routes/auth.$provider.redirect/route.tsx
@@ -9,19 +9,23 @@ export const loader: LoaderFunction = async ({
 }: LoaderFunctionArgs) => {
 	const { accountConfigured, backendIdentity, backendJwt } =
 		(await authenticator.isAuthenticated(request)) as CreatedSession;
+
 	const configured = await AuthService.checkUserState(backendJwt);
 	const session = await getSession(request.headers.get('Cookie'));
-	const { profile } = session.get('__session') as CreatedSession;
 
-	console.log('profile', profile);
-	console.log('configured', configured);
+	const existingSession = session.get('__session') as CreatedSession;
+
+	// configured must be inside of profile object.
+	Object.assign(existingSession.profile, { configured });
+
+	session.set('__session', existingSession);
 
 	return redirect(
-		`/?new=user&profile_name=${backendIdentity}${!accountConfigured ? '&registered=false' : ''}&source=${params.provider}`
-		// {
-		// 	headers: {
-		// 		'Set-Cookie': await commitSession(session),
-		// 	},
-		// }
+		`/?new=user&profile_name=${backendIdentity}${!accountConfigured ? '&registered=false' : ''}&source=${params.provider}`,
+		{
+			headers: {
+				'Set-Cookie': await commitSession(session),
+			},
+		}
 	);
 };
